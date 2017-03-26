1d18fc2d0db47ec927ed2bd9b6c3a93997f3ad67
codespert <prady.bissa@yahoo.co.in>
2017-03-26 10:18:18 +0530

    Create TB_Booth.v

---
+++ TB_Booth.v
@@ -0,0 +1,13 @@
+module TB_Booth();
+wire [16:0]out;
+reg [8:0]a,b;
+
+Booth_Main #(.n(8))uut(.out(out),.a(a),.b(b));
+
+initial
+begin
+#5 a=8'd10;b=8'd10;
+#15 $display($time," out=%d ",out);
+//#200 $finish;
+end
+endmodule
